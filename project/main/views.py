0e612b95faee7656d0d44bbdf7aac23ac9fc13e8
Leo Huang <crazysheep2302@gmail.com>
2015-03-04 00:51:45 +1100

    Add sample events feed, fetching from facebook

--- project/main/views.py
+++ project/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import requests
 #from django.http import HttpResponse
 
 # Create your views here.
@@ -6,7 +7,13 @@ def home(request):
     return render(request, 'home.html')
 
 def events(request):
-    return render(request, 'events.html')
+    params = {
+        'access_token': 'CAACEdEose0cBAFj3J4KFxZAJyMoR90iBQ5KPjrQun5ZCfzqivnMZA3UYTDjyYfKhkcUVapjCF9mxXgZBgkaEoWnyH5ag4CYlrnZAChXgBrnNcMnvoJKjtvxpxH87qSVLSeCb8rA4Iq1yQykYutdVfQ535ZBid7kjJldgvJWGZCv6jcxKPl5JHtdL15NsHRudS52lYrkzOzlrMhVmuATraBvXtbPUXZCzJLoZD',
+        'since': 0
+    }
+    data = requests.get('https://graph.facebook.com/185233411589007/events', params=params)
+    events = data.json()['data']
+    return render(request, 'events.html', {'events': events})
 
 def contact(request):
     return render(request, 'contact.html')
