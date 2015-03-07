382418af5f0bed8e85d29dfb611bd705974cd26b
Davy <e_equals_mass_speed_light_squared@hotmail.com>
2015-03-07 17:29:58 +1100

    Minor refactoring of code, fixed broken imports

--- project/main/urls.py
+++ project/main/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 
-from main import views
+import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^(?:home)?$', views.home, name='home'),
     url(r'^events$', views.events, name='events'),
     url(r'^contact$', views.contact, name='contact'),
--- project/main/views.py
+++ project/main/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 import requests
-#from django.http import HttpResponse
 
-# Create your views here.
+TEMP_ACCESS_TOKEN = 'CAACEdEose0cBAFj3J4KFxZAJyMoR90iBQ5KPjrQun5ZCfzqivnMZA3UYTDjyYfKhkcUVapjCF9mxXgZBgkaEoWnyH5ag4CYlrnZAChXgBrnNcMnvoJKjtvxpxH87qSVLSeCb8rA4Iq1yQykYutdVfQ535ZBid7kjJldgvJWGZCv6jcxKPl5JHtdL15NsHRudS52lYrkzOzlrMhVmuATraBvXtbPUXZCzJLoZD'
+
+
 def home(request):
     return render(request, 'home.html')
 
+
 def events(request):
     params = {
-        'access_token': 'CAACEdEose0cBAFj3J4KFxZAJyMoR90iBQ5KPjrQun5ZCfzqivnMZA3UYTDjyYfKhkcUVapjCF9mxXgZBgkaEoWnyH5ag4CYlrnZAChXgBrnNcMnvoJKjtvxpxH87qSVLSeCb8rA4Iq1yQykYutdVfQ535ZBid7kjJldgvJWGZCv6jcxKPl5JHtdL15NsHRudS52lYrkzOzlrMhVmuATraBvXtbPUXZCzJLoZD',
+        'access_token': TEMP_ACCESS_TOKEN,
         'since': 0
     }
     data = requests.get('https://graph.facebook.com/185233411589007/events', params=params)
