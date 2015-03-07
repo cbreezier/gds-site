aac76d153366844dc951c1a2ffa617c2f09dab37
Davy <e_equals_mass_speed_light_squared@hotmail.com>
2015-03-07 22:56:39 +1100

    Use raw names for URL import

--- project/main/urls.py
+++ project/main/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-import views
+from main import views
 
 urlpatterns = patterns(
     '',
--- project/project/urls.py
+++ project/project/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+import main.urls
+
 urlpatterns = patterns(
     '',
-    url(r'', include('main.urls')),
+    url(r'', include(main.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
