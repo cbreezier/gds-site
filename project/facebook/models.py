391350d0fa5c67e7fcba52aef3ac05a093ba53c5
Davy <e_equals_mass_speed_light_squared@hotmail.com>
2015-03-09 11:33:18 +1100

    Added event model

--- project/facebook/models.py
+++ project/facebook/models.py
@@ -1,9 +1,29 @@
 from django.db import models
+from django.conf import settings
 import requests
 
 
 class FacebookEvent(models.Model):
-    pass
+    event_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=1024)
+    description = models.TextField()
+    start_time = models.DateField()
+    end_time = models.DateField()
+    location = models.CharField(max_length=1024)
+
+    @staticmethod
+    def update_events(session, group_id):
+        params = {
+            'access_token': session.access_token,
+            'since': 0,  # All events since epoch
+        }
+        events = requests.get("https://graph.facebook.com/v2.2/{}/events".format(settings.FACEBOOK_GROUP_ID),
+                              params=params)
+        pass
+
+
+# Start a worker to update the event list every 5 minutes
+pass
 
 
 class FacebookSession(models.Model):
