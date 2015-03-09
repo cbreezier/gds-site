8d32eda4d26ca7796c6f7d2018b7b22d5b48595b
Davy <e_equals_mass_speed_light_squared@hotmail.com>
2015-03-09 11:30:17 +1100

    Renamed facebook module

--- project/facebook/admin.py
+++ project/facebook/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.db import models
 
-from .models import FacebookAccessToken
+from .models import FacebookSession
 from .forms import FacebookAccessInput
 
 
-@admin.register(FacebookAccessToken)
+@admin.register(FacebookSession)
 class FacebookAccessAdmin(admin.ModelAdmin):
     list_display = ('short_token', 'is_valid',)
     formfield_overrides = {
--- project/facebook/models.py
+++ project/facebook/models.py
@@ -1,13 +1,18 @@
 from django.db import models
+import requests
 
 
-class FacebookAccessToken(models.Model):
+class FacebookEvent(models.Model):
+    pass
+
+
+class FacebookSession(models.Model):
     SHORT_TOKEN_LENGTH = 10
 
     access_token = models.CharField(max_length=1024)
 
     def short_token(self):
-        return self.access_token[:FacebookAccessToken.SHORT_TOKEN_LENGTH] + "..."
+        return self.access_token[:FacebookSession.SHORT_TOKEN_LENGTH] + "..."
 
     def is_valid(self):
         if self == self:
